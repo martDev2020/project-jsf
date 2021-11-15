032765e8e7224416cf241f1d96cfd9172139f79e
Martín <al221310188@gmail.com>
2021-11-15 17:17:11 -0600

    Nombre del proyecto: Project-JSF verión 1.1 Descripción: se crea el aplicativo y se direcciona a la pantalla de principal.

--- src/main/java/com/devmart/projectjsf/controllers/LoginController.java
+++ src/main/java/com/devmart/projectjsf/controllers/LoginController.java
@@ -3,7 +3,12 @@
  */
 package com.devmart.projectjsf.controllers;
 
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 /**
  * @author Martín Clase que permite intereactuar con la visa d login.xhtml.
@@ -13,14 +18,29 @@ import javax.faces.bean.ManagedBean;
 public class LoginController {
 	private String usuario;
 	private String password;
-	
+
 	public void ingresar() {
-		System.out.println("Usuario: " + usuario);	
+		System.out.println("Usuario: " + usuario);
+		if (usuario.equals("devmart") && password.equals("123")) {
+			try {
+				this.redireccionar("principal.xhtml");
+			} catch (IOException e) {
+				FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario",
+						new FacesMessage(FacesMessage.SEVERITY_FATAL, "La página no existe.", ""));
+				e.printStackTrace();
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario",
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario y/o contraseña incorrectos.", ""));
 		}
+	}
+	private void redireccionar(String pagina) throws IOException {
+		ExternalContext ec= FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(pagina);
+	}
 
 	/**
-	 * @return the usuario
-	 * Usuario que entra al login.
+	 * @return the usuario Usuario que entra al login.
 	 */
 	public String getUsuario() {
 		return usuario;
@@ -34,8 +54,7 @@ public class LoginController {
 	}
 
 	/**
-	 * @return the password
-	 * Passwor que entra al login.
+	 * @return the password Passwor que entra al login.
 	 */
 	public String getPassword() {
 		return password;
